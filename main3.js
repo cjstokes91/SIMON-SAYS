a4f9a0c2f050dc4c0f41281b6db2313a5b125841
cjstokes91 <59268316+cjstokes91@users.noreply.github.com>
2020-02-21 17:01:37 -0800

    passing project

--- main3.js
+++ main3.js
@@ -4,34 +4,29 @@ const topRight = document.querySelector('#p1');
 const bottomLeft = document.querySelector('#p2');
 const bottomRight = document.querySelector('#p3');
 const colorPad = document.querySelector('.pads');
-const onButton = document.querySelector('#hard');
 const turnCounter = document.querySelector('#turn');
-const clips = ['clip1', 'clip2'];
-const sounds = ['https://s3.amazonaws.com/freecodecamp/simonSound1.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'];
-const audio = new Audio();
+const sounds = ['https://s3.amazonaws.com/freecodecamp/simonSound1.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'];
+// const audio = new Audio();
 
 
 let order = [];
+let ranOrder = [];
 let userOrder = [];
 let lightUp, compTurn, positive;
 let win;
 let turn;
 let hard = false;
 let noise = true;
-let maxMoves = 3;
-let on = false;
+let maxMoves;
 let gameStarted;
-let ranNum;
+// let ranNum;
 let mostRecentClick = -1;
 let keepGoing = true;
+let sequenceNumer = 0
 let msg = document.querySelector('#msg');
 
 
 let startButton = document.querySelector('#start');
-let hardButton = document.querySelector('#on');
-
-
-
 startButton.addEventListener('click', playGame)
 
 function playGame() {
@@ -39,23 +34,30 @@ function playGame() {
     getSequence();
 }
 
-document.querySelector('#p0').addEventListener('click', handleClick);
-document.querySelector('#p1').addEventListener('click', handleClick);
-document.querySelector('#p2').addEventListener('click', handleClick);
-document.querySelector('#p3').addEventListener('click', handleClick);
+document.querySelector('#p0').addEventListener('click', handleClick2);
+document.querySelector('#p1').addEventListener('click', handleClick2);
+document.querySelector('#p2').addEventListener('click', handleClick2);
+document.querySelector('#p3').addEventListener('click', handleClick2);
 
-
+function startGame() {
+    if (startButton === true) {
+        // handleClick();
+    }
+    else startButton = false;
+}
 
 function handleClick(e) {
-
+    if (startButton == false) {
+        return
+    }
     if (e.target.id == `p0`) {
-        topLeft.style.backgroundColor = 'violet';
+        topLeft.style.backgroundColor = 'pink';
         userOrder.push([0]);
-        audio.src = sounds[1];
-        audio.play();
+        // audio.src = sounds[1];
+        // audio.play();
         mostRecentClick = 0;
         if (ranNum === mostRecentClick) {
-                msg.innerHTML = 'correct!!!';
+            msg.innerHTML = 'correct!!!';
 
 
             getSequence();
@@ -68,107 +70,175 @@ function handleClick(e) {
 
     if (e.target.id == `p1`) {
         topRight.style.backgroundColor =
-            'skyblue';
-        audio.src = sounds[0];
+            'lightblue';
+        // audio.src = sounds[0];
         userOrder.push([1]);
-        audio.play();
+        // audio.play();
         mostRecentClick = 1;
         if (ranNum === mostRecentClick) {
-            msg.innerHTML = 'correct!!!';
+            msg.innerHTML = 'nice!!!';
             getSequence();
 
         } else {
             setTimeout(() => {
-                msg.innerHTML = "nope!!";
+                msg.innerHTML = "whoops!!, you lose";
 
             }, 100)
         }
     }
     if (e.target.id == `p2`) {
         bottomLeft.style.backgroundColor = 'lightyellow';
-        audio.src = sounds[1];co
+        // audio.src = sounds[1];
         userOrder.push([2]);
-        audio.play();
+        // audio.play();
         mostRecentClick = 2;
         if (ranNum === mostRecentClick) {
-            msg.innerHTML = 'correct!!!';
+            msg.innerHTML = 'you did it !!!';
             getSequence();
         } else {
-            msg.innerHTML = "nope!!";
+            msg.innerHTML = "nope!!, you lose";
         }
 
     }
     if (e.target.id == `p3`) {
         bottomRight.style.backgroundColor = 'darkgreen';
-        audio.src = sounds[1];
+        // audio.src = sounds[1];
         userOrder.push([3]);
-        audio.play();
+        // audio.play();
         mostRecentClick = 3;
         if (ranNum === mostRecentClick) {
-            msg.innerHTML = 'correct!!!';
+            msg.innerHTML = 'keep going!!!';
             getSequence();
 
         } else {
-            msg.innerHTML = "nope!!";
+            msg.innerHTML = "whoops!! you lose";
         }
     }
 
+    if (e.target.id == `p0`) {
+        topLeft.style.backgroundColor = 'violet';
+        // audio.src = sounds[1];
+        userOrder.push([0]);
+        // audio.play();
+        mostRecentClick = 0;
+        if (ranNum === mostRecentClick) {
+            msg.innerHTML = "good job!!";
+            getSequence();
 
-    setTimeout(function () {
-        topLeft.style.backgroundColor = 'purple';
-        topRight.style.backgroundColor = 'blue';
-        bottomLeft.style.backgroundColor = 'yellow';
-        bottomRight.style.backgroundColor = 'green';
-    }, 300);
-
-
+        } else {
+            msg.innerHTML = "sorry!! , thats not it";
+        }
+    }
 }
+
+function handleClick2(e){
+    if (startButton == false) {
+        return
+    }
+    console.log(order)
+    if (e.target.id == `p0`) {
+        if(order[0] == 0){
+            msg.textContent = "YOU WIN"
+            // at some point, we will reincorporate getSequence to make "order" longer each turn
+            // getSequence()
+        } else {
+            msg.textContent = "YOU LOSE"
+        }
+    }
+    if (e.target.id == `p1`) {
+        if(order[0] == 1){
+            msg.textContent = "YOU WIN"
+            // at some point, we will reincorporate getSequence to make "order" longer each turn
+            // getSequence()
+        } else {
+            msg.textContent = "YOU LOSE"
+        }
+    }
+    if (e.target.id == `p2`) {
+        if(order[0] == 2){
+            msg.textContent = "YOU WIN"
+            // at some point, we will reincorporate getSequence to make "order" longer each turn
+            // getSequence()
+        } else {
+            msg.textContent = "YOU LOSE"
+        }
+    }
+    if (e.target.id == `p3`) {
+        if(order[0] == 3){
+            msg.textContent = "YOU WIN"
+            // at some point, we will reincorporate getSequence to make "order" longer each turn
+            // getSequence()
+        } else {
+            msg.textContent = "YOU LOSE"
+        }
+    }
+    // this makes it such that the "order" will always only consist of one light, never more.
+    order = []
+}
+
+
+setTimeout(function () {
+    topLeft.style.backgroundColor = 'purple';
+    topRight.style.backgroundColor = 'blue';
+    bottomLeft.style.backgroundColor = 'yellow';
+    bottomRight.style.backgroundColor = 'green';
+}, 100);
+
+
+
 
 
 function getSequence() {
     ranNum = Math.floor(Math.random() * 4);
-    console.log("im in get sequence pick number " + ranNum);
+    console.log("p " + ranNum);
     order.push(ranNum);
+    // let showLights = setTimeout(lightSequence, 2000)
+    // showLights()
     lightSequence();
+    //for (var i = 0; i < 20; i++) {
+    //order.push(Math.floor(Math.random() * 4) + 1);
+    // push new sequence to ranOrder 
 
 
 }
 
 function lightSequence() {
     on = true;
-    order.forEach(function (index, indexSeq) {
+    order.forEach(function (code, indexSeq) {
+        let saveColor;
         setTimeout(() => {
-            let pad = document.getElementById('p' + index)
-            pad.className += ' activated';
-            audio.src = sounds[indexSeq];
-            audio.play();
+            let pad = document.getElementById('p' + code);
+            saveColor = pad.style.backgroundColor;
+            pad.style.backgroundColor = 'white';
+            // audio.src = sounds[code];
+            // audio.play();
             setTimeout(() => {
-                pad.classList.remove('activated');
-                audio.pause();
-            }, 300)
-
-            console.log('pad');
-
-        }, 400)
+                pad.style.backgroundColor = saveColor;
+            }, 400)
+        }, 500);
     });
+    
 }
 
+function clearColor() {
+    topLeft.style.backgroundColor = "darkgreen";
+    topRight.style.backgroundColor = "darkred";
+    bottomLeft.style.backgroundColor = "goldenrod";
+    bottomRight.style.backgroundColor = "darkblue";
+
+}
 
 function checkWin() {
-    if (userOrder[0][1][2] === order[0][1][2]) {
-
-        getWinnwer();
-    }
-    if (!userOrder === order) {
-
-    }
-
+    //check if playerOrder is === to ranOrder 
+    // if user order === ranOrder and if turn is 5 
+    //getWinner(); 
+    // if true keep playing game 
+    // if false render lose message 
 }
-
 function getWinner() {
-    let congrats;
-    let win = true;
+    //render win message 
+    //clear board 
+
 
 }
-
 
